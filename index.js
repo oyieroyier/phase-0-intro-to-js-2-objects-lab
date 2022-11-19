30fde7fc9105e538f2f698897cf0598bc6a07dd3
Bob Oyier <boboyier92@gmail.com>
2022-11-19 17:46:06 +0300

    "Done with the Objects Lab."

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+  name: "Bob",
+  address: "San Francisco",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value,
+  };
+}
+
+// const newEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Sammy");
+// console.log(newEmployee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+// updateEmployeeWithKeyAndValue(employee, "name", "Sam");
+// console.log(employee);
+
+function deleteFromEmployeeByKey(obj, key) {
+  const deletedEmployee = { ...obj };
+  delete deletedEmployee[key];
+  return deletedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+	delete obj[key];
+	return obj
+}
